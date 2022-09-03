5a79660830c026b265820ff32e53cc945f683395
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-09-02 23:53:17 -0500

    Update 6-hogwarts.js

---
+++ 0x0C-Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,44 @@
+class studentHogwart {
+  constructor() {
+    this.privateScore = 0;
+    this.name = null;
+  }
+
+  /* Private method changeScoreBy */
+  changeScoreBy(points) {
+    this.privateScore += points;
+  }
+
+  /*The module gives access to four public methods (return an object)*/
+  setName(newName) {
+    this.name = newName;
+  }
+
+  rewardStudent() {
+    this.changeScoreBy(1);
+  }
+
+  penalizeStudent() {
+    this.changeScoreBy(-1);
+  }
+
+  getScore() {
+    return this.name + ": " + this.privateScore;
+    // return `${this.name}: ${this.privateScore}`
+  }
+}
+
+let harry = new studentHogwart();
+harry.setName("Harry");
+for (let i = 0; i < 4; i++) {
+  harry.rewardStudent()
+}
+console.log(harry.getScore());
+
+let draco = new studentHogwart();
+draco.setName("Draco");
+draco.rewardStudent();
+for (let i = 0; i < 3; i++) {
+  draco.penalizeStudent()
+}
+console.log(draco.getScore());
