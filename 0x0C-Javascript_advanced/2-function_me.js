b3c1457fe9aa16a105f8ab02c8b6dad6dd3564f9
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-09-02 23:50:42 -0500

    Update 2-function_me.js

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+function welcomeMessage(fullName) {
+  function closure() {
+    alert(`Welcome ${fullName}`);
+  }
+
+  return closure;
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
